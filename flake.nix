22e5db957eb116ab3bd409edaa83dc663410abfa
Minsub Kim <rapidfsub@gmail.com>
2023-09-14 11:20:44 +0900

    Add nix darwin module

--- flake.nix
+++ flake.nix
@@ -26,70 +26,6 @@
           "vscode"
         ];
       };
-
-      configuration = { ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages =
-          [
-            pkgs.vim
-          ];
-
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
-        # nix.package = pkgs.nix;
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true; # default shell on catalina
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "x86_64-darwin";
-
-        environment = {
-          shellAliases = {
-            ds = "darwin-rebuild switch --flake ~/.config/home-manager";
-            hs = "home-manager switch --flake ~/.config/home-manager";
-          };
-        };
-
-        homebrew = {
-          enable = true;
-          brews = [
-            "mas"
-          ];
-          taps = [
-            "homebrew/bundle"
-            "homebrew/cask-fonts"
-          ];
-          casks = [
-            "firefox"
-            "font-fantasque-sans-mono-nerd-font"
-            "karabiner-elements"
-            "microsoft-teams"
-            "raycast"
-            "scroll-reverser"
-            "setapp"
-            "warp"
-          ];
-          masApps = {
-            KakaoTalk = 869223134;
-          };
-          onActivation = {
-            cleanup = "zap";
-          };
-        };
-      };
     in
     {
       homeConfigurations."rapidfsub" = home-manager.lib.homeManagerConfiguration {
@@ -106,7 +42,13 @@
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#rapidfsub-2017
       darwinConfigurations."rapidfsub-2017" = nix-darwin.lib.darwinSystem {
-        modules = [ configuration ];
+        inherit pkgs;
+
+        modules = [ ./configuration.nix ];
+
+        specialArgs = {
+          root = self;
+        };
       };
 
       # Expose the package set, including overlays, for convenience.
