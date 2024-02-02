22ce95ea63c7e09406b2aa274d8685c08e3fc3da
Minsub Kim <rapidfsub@gmail.com>
2024-02-02 18:05:51 +0900

    Revert to vscode

--- home/vscode.nix
+++ home/vscode.nix
@@ -15,15 +15,15 @@ in
     enableExtensionUpdateCheck = false;
     extensions = map getPackage extensions.recommendations;
     keybindings = importJSON ./.config/vscode/keybindings.json;
-    package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
-      src = (fetchTarball {
-        url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-universal";
-        sha256 = "1j33g5c2hjcgbdk7s9607g9ap7v0h6lpdsg51xwd80wsf3hr93d9";
-      });
-      version = "latest";
-
-      buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
-    });
+    package = pkgs.vscode;
+    # package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
+    #   src = (fetchTarball {
+    #     url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-universal";
+    #     sha256 = "1j33g5c2hjcgbdk7s9607g9ap7v0h6lpdsg51xwd80wsf3hr93d9";
+    #   });
+    #   version = "latest";
+    #   buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
+    # });
     userSettings = importJSON ../.vscode/settings.json;
     globalSnippets = {
       liveview = importJSON ./.config/vscode/liveview.code-snippets;
